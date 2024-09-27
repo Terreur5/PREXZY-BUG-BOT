161aba4d323246f5efef13534b98670eaa5ece75
𝑴𝒖𝒛𝒂𝒏-md <tcheuffapresca@gmail.com>
2024-09-27 11:17:51 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,8 +8,8 @@ global.ytname = "YT: Prexzyvilla"
 global.socialm = "GitHub: Prexzybooster"
 global.location = "Nigeria, Osun, Osogbo"
 
-global.ownernumber = '263717750825'  //creator number
-global.ownername = 'Precious Ayomide👑' //owner name
+global.ownernumber = '393716907857'  //creator number
+global.ownername = 'muzan sukuna 👑' //owner name
 global.botname = 'PREXZY ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
 
 //sticker details
